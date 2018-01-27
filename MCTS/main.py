923420918bb384153a093392b057732ff329e928
Jay Whang <jaywhang@stanford.edu>
2018-01-27 23:17:26 +0000

    Using more sensible experiment directory name for MCTS

--- MCTS/main.py
+++ MCTS/main.py
@@ -4,6 +4,7 @@ import numpy as np
 from model import GymModel
 from mcts import MCTS
 
+
 def run_episode(
     env_name, max_horizon=None, max_depth=None, num_rollouts=None,
     action_map=None, render=False):
@@ -44,27 +45,28 @@ def main():
   # Parse arguments
   parser = argparse.ArgumentParser(description='Run MCTS experiments.')
   parser.add_argument('env_name', help='Name of the Gym environment')
-  parser.add_argument('-d', '--max_depth', type=int, default=300,
+  parser.add_argument('-md', '--max_depth', type=int, default=300,
                       help='Max rollout depth')
-  parser.add_argument('-r', '--num_rollouts', type=int, default=500,
+  parser.add_argument('-nr', '--num_rollouts', type=int, default=500,
                       help='Number rollouts to use to plan a single step')
-  parser.add_argument('-l', '--max_horizon', type=int, default=None,
+  parser.add_argument('-mh', '--max_horizon', type=int, default=None,
                       help='Max number of steps before episode is stopped')
-  parser.add_argument('-e', '--num_episodes', type=int, default=50,
+  parser.add_argument('-ne', '--num_episodes', type=int, default=50,
                       help='Number of episodes to run the experiment for')
   args = parser.parse_args()
 
   # Set up experiment directory and log file
-  dir_name = '%s_%s' % (args.env_name, time.strftime('%Y%m%d_%H%M%S'))
-  if not os.path.exists(dir_name):
-    os.makedirs(dir_name)
+  exp_name = 'nr_%d.ne_%d.md_%d.mh_%s' % (args.num_rollouts, args.num_episodes,
+          args.max_depth, str(args.max_horizon))
+  dir_name = '%s.%s' % (args.env_name, exp_name)
+  if not os.path.exists(dir_name): os.makedirs(dir_name)
   logfile = open('%s/log.txt' % dir_name, 'w')
   def writelog(msg):
     logfile.write(msg + '\n')
     logfile.flush()
   writelog('Hyperparameters')
   writelog('\n'.join([('  %15s: %s' % ('--'+arg, str(val)))
-    for arg, val in vars(args).iteritems()]))
+    for arg, val in vars(args).iteritems()]) + '\n')
 
   scores = []
   for ep in range(1, args.num_episodes+1):
@@ -74,8 +76,8 @@ def main():
         max_horizon=args.max_horizon,
         num_rollouts=args.num_rollouts,
         max_depth=args.max_depth)
-    writelog('Ep %03d / %03d finished in %d steps (%.2f sec)'
-        % (ep, args.num_episodes, steps, time.time()-ep_start_time))
+    writelog('ep %03d/%03d finished in %d steps (%.2f sec) with return %.2f' % (
+        ep, args.num_episodes, steps, time.time()-ep_start_time, sum(rewards)))
     np.save('%s/ep_%03d_rewards.npy' % (dir_name, ep), np.array(rewards))
     np.save('%s/ep_%03d_actions.npy' % (dir_name, ep), np.array(actions))
 
